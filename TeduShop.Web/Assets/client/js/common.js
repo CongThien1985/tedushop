3b5587d900b97cbb32de52fe1db6b3016d6d90b4
congthien1985 <congthien.en@gmail.com>
2019-04-04 21:15:11 +0700

    create order and validation

--- TeduShop.Web/Assets/client/js/common.js
+++ TeduShop.Web/Assets/client/js/common.js
@@ -31,6 +31,23 @@
                 .appendTo(ul);
         };
 
+        $('.btnAddToCart').off('click').on('click', function (e) {
+            e.preventDefault();
+            var productId = parseInt($(this).data('id'));
+            $.ajax({
+                url: '/ShoppingCart/Add',
+                data: {
+                    productId: productId
+                },
+                type: 'POST',
+                dataType: 'json',
+                success: function (response) {
+                    if (response.status) {
+                        alert('Thêm sản phẩm thành công.');
+                    }
+                }
+            });
+        });
         $('#btnLogout').off('click').on('click', function (e) {
             e.preventDefault();
             $('#frmLogout').submit();
